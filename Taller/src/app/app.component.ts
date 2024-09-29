74f08199b06c79b1c514fb9903317fb4a13d0235
Jorge-Gaston <pollotrabaja@gmail.com>
2024-09-29 00:35:36 -0600

    Feature: I add the proyect

---
+++ Taller/src/app/app.component.ts
@@ -0,0 +1,24 @@
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  verMas(){
+    alert("¿Quieres ver más? 👀👀")
+  }
+
+  conocerMas(){
+    alert("¿Quieres conocer más? 🥵🥵")
+  }
+
+
+}
